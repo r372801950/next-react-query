3b43fa9d44465548f4b36eb4c1dda5ce9ee76b8a
jiang <372801950@qq.com>
2025-04-22 18:03:52 +0800

    img=>Image priority

--- src/app/pokemon/pokemon-info.tsx
+++ src/app/pokemon/pokemon-info.tsx
@@ -3,16 +3,17 @@
 import React from 'react'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { pokemonOptions } from '@/app/pokemon'
+import Image from 'next/image'
 
 export function PokemonInfo() {
   const { data } = useSuspenseQuery(pokemonOptions)
-  console.log('PokemonInfo 渲染环境:', typeof window === 'undefined' ? '服务器' : '客户端')
+  // console.log('PokemonInfo 渲染环境:', typeof window === 'undefined' ? '服务器' : '客户端')
 
   return (
     <div>
       <figure>
-        <img src={data.sprites.front_shiny} height={200} alt={data.name} />
-        <h2>I'm {data.name}</h2>
+        <Image src={data.sprites.front_shiny} width={200} height={200} alt={data.name} priority />
+        <h2>I&apos;m {data.name}</h2>
       </figure>
     </div>
   )
